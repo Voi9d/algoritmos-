12cba68dae35fd78d0df677374530789edadf49d
IzabelyBrumR <103642004+IzabelyBrumR@users.noreply.github.com>
2022-04-28 13:57:22 -0300

    primeiro commit

---
+++ algortimos.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+intx,y,soma;// Declaração de variáveis
+printf("entre com dois números");
+scanf("%d",&x);
+scanf("%d",&y);
+soma=x+y;
+printf("O resultado da soma:%d",soma);
+}
